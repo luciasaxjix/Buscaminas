30f01c40780bca8145dec111eea29e1c0d08e3f9
JeremySO202 <jeremyserracin@gmail.com>
2023-03-22 12:48:25 -0600

    Initial commit

---
+++ src/main/java/buscaminas/Buscaminas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package buscaminas;
+
+import Ventanas.VentanaJuego;
+
+/**
+ *
+ * @author jerem
+ */
+public class Buscaminas {
+
+    public static void main(String[] args) {
+        VentanaJuego r = new VentanaJuego();
+        r.setVisible(true);
+    }
+}
